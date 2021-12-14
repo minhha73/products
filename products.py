83bed1687b5f8b8bbe0cb22f092d7dd0b7d34b7a
Minh Ha <minhha.hoang@hotmail.com>
2021-12-14 16:32:13 +0800

    Add read file

--- products.py
+++ products.py
@@ -15,9 +15,9 @@ while True :
 		break
 	price = int(input('請輸入商品價格： '))
 	products.append([ name, price])
-	
+
 #寫入檔案
 with open ('products.csv','w',encoding='utf-8') as f:
 	f.write('商品,價格' '\n')
 	for p in products :
-		f.write(p[0] + ',' + str(p[1]) + '\n')
+ 		f.write(p[0] + ',' + str(p[1]) + '\n')
