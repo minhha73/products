f988c38fca4356161197ee5e25fac818f9919cc9
Minh Ha <minhha.hoang@hotmail.com>
2021-12-14 16:28:05 +0800

    Add read File

--- products.py
+++ products.py
@@ -1,18 +1,23 @@
+#讀取檔案
 products = []
+with open ('products.csv' ,'r', encoding='utf-8') as f :
+	for line in f :
+		if '商品,價格' in line :
+			continue #繼續
+		name, price = line.strip().split(',') #strip清楚空白換行 split切割欄位
+		products.append([name ,price])
+print(products)
+
+#讓使用者輸入
 while True :
 	name = input('請輸入商品：')
 	if name == 'q':
 		break
-	price = input('請輸入商品價格： ')
+	price = int(input('請輸入商品價格： '))
 	products.append([ name, price])
-#print(products)
-#for p in products:
-#	print(p[0], '的價格為 :' ,p[1])
+	
+#寫入檔案
 with open ('products.csv','w',encoding='utf-8') as f:
+	f.write('商品,價格' '\n')
 	for p in products :
-		f.write(p[0] + ',' + p[1] + '\n')
-# 把清單中的整數寫進test.txt
-#data=[2, 4, 6, 8, 10]
-#with open ('test.txt', 'w') as f :
-#	for d in data :
-#		f.write(str(d) + '\n')
+		f.write(p[0] + ',' + str(p[1]) + '\n')
